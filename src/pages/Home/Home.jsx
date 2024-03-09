a59cc9e0b9cd2eaca58fd945566b6d0643900491
Samuel Gleize-Bourras <samuel@shunpo.fr>
2024-03-09 22:16:01 +0100

    Home page creation

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,21 @@
 import './Home.scss'
+import Header from "../../components/Header/Header"
+import Footer from "../../components/Footer/Footer"
+import Hero from "../../components/Hero/Hero"
+import Thumb from "../../components/Thumb/Thumb"
+
 
 function Home() {
     return (
         <div>
-            <h1>Home</h1>
+            <div className='wrapper'>
+                <Header />
+                <main>
+                    <Hero />
+                    <Thumb />
+                </main>
+            </div>
+            <Footer />
         </div>
     )
 }
