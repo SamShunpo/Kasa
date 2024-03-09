7eef97406ef1d3f2be7ddff980c9d23825cd3a45
Samuel Gleize-Bourras <samuel@shunpo.fr>
2024-03-09 22:18:36 +0100

    Index Upload React V18

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom/client'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Location from './pages/Location/Location'
 import Home from './pages/Home/Home'
 import About from './pages/About/About'
+import "@fontsource/montserrat"
+
  
-ReactDOM.render(
+ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <Router>
             <Routes>
@@ -15,5 +17,4 @@ ReactDOM.render(
             </Routes>
                   </Router>
     </React.StrictMode>,
-document.getElementById('root')
 )
