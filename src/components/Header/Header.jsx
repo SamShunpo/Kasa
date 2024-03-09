a434dfe4e4a286be61e77a5613e44da9f1705bf6
Samuel Gleize-Bourras <samuel@shunpo.fr>
2024-03-09 22:16:37 +0100

    Header component creation

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,16 @@
-import 
+import './Header.scss'
+import {Link} from 'react-router-dom'
+import logo from '../../assets/logo_red.png'
 
 function Header() {
 return (
-    <div>Header</div>
+    <header className='kasa-header'>
+        <img src={logo} alt="Logo Kasa rouge" />
+        <nav>
+            <Link to="/">Accueil</Link>
+            <Link to="/About">A Propos</Link>
+        </nav>
+    </header>
 )
 }
 
