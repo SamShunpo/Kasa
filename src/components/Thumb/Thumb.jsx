8759a6bcf8a39433588ecbb3b4da6e4b54ee376f
Samuel Gleize-Bourras <samuel@shunpo.fr>
2024-03-25 09:10:10 +0100

    Thumb Update

--- src/components/Thumb/Thumb.jsx
+++ src/components/Thumb/Thumb.jsx
@@ -1,18 +1,14 @@
-import location from '../../datas/locations.json';
 import './Thumb.scss';
 
-function Thumb() {
+function Thumb({cover, title}) {
     return (
-        <section className='kasa-thumb'>
-            {location.map(({ cover, title, id }) => (
-                <article key={id} style={{
+        //<section className='kasa-thumb'>
+                <article style={{
                     backgroundImage: `linear-gradient(180deg, rgba(255, 255, 255, 0.00) 0%, rgba(10, 10, 10, 0.00) 42%, rgba(4, 4, 4, 0.50) 80%, rgba(0, 0, 0, 0.70) 100%), url(${cover}`,
                 }}>
                     <p>{title}</p>
                 </article>
-            ))}
-        </section>
-    );
-}
+            )}
+        //</section>
 
 export default Thumb
