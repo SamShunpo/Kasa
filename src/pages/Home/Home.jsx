45c0ad93f4098084d4e654d539c11cb0eb00ec41
Samuel Gleize-Bourras <samuel@shunpo.fr>
2024-03-25 09:42:19 +0100

    Add Hero and Thumb

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,16 +3,38 @@ import Header from "../../components/Header/Header"
 import Footer from "../../components/Footer/Footer"
 import Hero from "../../components/Hero/Hero"
 import Thumb from "../../components/Thumb/Thumb"
+import { useState, useEffect } from "react"
+import { Link } from 'react-router-dom'
+import img1 from '../../assets/img1.png'
 
 
 function Home() {
+
+    const [locations, setLocationsData] = useState([]);
+
+    useEffect(() => {
+        const getLocationList = async () => {
+            const reponse = await fetch("http://localhost:5173/datas/locations.json");
+            const location = await reponse.json();
+            setLocationsData(location);
+        }
+        getLocationList();
+    }, []);
+
+
     return (
         <div>
             <div className='wrapper'>
                 <Header />
                 <main>
-                    <Hero />
-                    <Thumb />
+                    <Hero img={img1} title='Chez vous, partout et ailleurs'/>
+                    <section>
+                        {locations.map(({cover, title, id}) =>
+                            <Link key={id} to={`/location/${id}`} className='link'>
+                                <Thumb cover={cover} title={title} />
+                            </Link>
+                        )}
+                    </section>
                 </main>
             </div>
             <Footer />
