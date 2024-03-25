fc12c980a0094cc7bce6d13d2ffbe4377a90daf9
Samuel Gleize-Bourras <samuel@shunpo.fr>
2024-03-25 09:39:49 +0100

    Update About with Hero

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -2,13 +2,14 @@ import './About.scss'
 import Header from "../../components/Header/Header"
 import Hero from "../../components/Hero/Hero"
 import Footer from "../../components/Footer/Footer"
+import img2 from '../../assets/img2.png'
 
 function About() {
     return (
         <div>
             <div className='wrapper'>
                 <Header />
-                <Hero />
+                <Hero img={img2}/>
             </div>
                 <Footer />
         </div>
