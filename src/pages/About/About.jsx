616d86120aba6b33da964965466108304a49261f
Samuel Gleize-Bourras <samuel@shunpo.fr>
2024-05-03 08:48:18 +0200

    About Page Creation

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,19 +1,40 @@
-import './About.scss'
-import Header from "../../components/Header/Header"
-import Hero from "../../components/Hero/Hero"
-import Footer from "../../components/Footer/Footer"
-import img2 from '../../assets/img2.png'
+import './About.scss';
+import React, { useState, useEffect } from 'react';
+import Header from "../../components/Header/Header";
+import Hero from "../../components/Hero/Hero";
+import Footer from "../../components/Footer/Footer";
+import img2 from '../../assets/img2.png';
+import Collapse from '../../components/Collapse/Collapse';
 
 function About() {
+
+    const [abouts, setAbouts] = useState([]);
+
+    useEffect(() => {
+        const getAbout = async () => {
+            const reponse = await fetch("http://localhost:5173/datas/about.json");
+            const data = await reponse.json();
+            setAbouts(data);
+        };
+        getAbout();
+    }, []);
+
     return (
         <div>
-            <div className='wrapper'>
-                <Header />
-                <Hero img={img2}/>
-            </div>
-                <Footer />
-        </div>
-    )
+            <Header />
+            <main>
+                <Hero img={img2} />
+                <div className='collapse'>
+                    {abouts.map((about, index) => (
+                        <Collapse key={index} title={about.title}>
+                            <p>{about.description}</p>
+                        </Collapse>
+                    ))}
+                </div>
+            </main>
+            <Footer />
+        </div >
+    );
 }
 
 export default About
