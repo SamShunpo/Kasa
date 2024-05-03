eb05abeb5f2ecab5c42b8582559ccf7feabbc58c
Samuel Gleize-Bourras <samuel@shunpo.fr>
2024-05-03 08:48:39 +0200

    Error page creation

---
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,23 @@
+import Header from "../../components/Header/Header"
+import Footer from "../../components/Footer/Footer"
+import {Link} from 'react-router-dom'
+import "./ErrorPage.scss"
+
+
+function ErrorPage() {
+
+    return (
+        <div>
+            <Header />
+            <main>
+                <h1>404</h1>
+                <p>Oups! La page que vous demandez n'existe pas.</p>
+                <Link to="/" className="link-error-page">Retourner sur la page d’accueil</Link>
+            </main>
+            <Footer />
+        </div>
+
+    )
+}
+
+export default ErrorPage
