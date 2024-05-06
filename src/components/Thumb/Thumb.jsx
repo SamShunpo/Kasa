8881b12ebaad51178f1d67fc465741af49466835
Samuel Gleize-Bourras <samuel@shunpo.fr>
2024-05-06 09:42:33 +0200

    Balise h1 modification

--- src/components/Thumb/Thumb.jsx
+++ src/components/Thumb/Thumb.jsx
@@ -6,7 +6,7 @@ function Thumb({cover, title}) {
                 <article style={{
                     backgroundImage: `linear-gradient(180deg, rgba(255, 255, 255, 0.00) 0%, rgba(10, 10, 10, 0.00) 42%, rgba(4, 4, 4, 0.50) 80%, rgba(0, 0, 0, 0.70) 100%), url(${cover}`,
                 }}>
-                    <p>{title}</p>
+                    <h1>{title}</h1>
                 </article>
             )}
         //</section>
